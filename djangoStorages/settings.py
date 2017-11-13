c6fce15821490c8a35ee5f2b383f1ab974f5fbf9
Falichev Aleksandr <6064233@gmail.com>
2017-11-13 11:12:53 +0800

    fix 4

--- djangoStorages/settings.py
+++ djangoStorages/settings.py
@@ -113,9 +113,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 
 # django-storages
 AWS_STORAGE_BUCKET_NAME = 'bucket_name'
